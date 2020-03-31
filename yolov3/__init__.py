40f090e6c4efcacdcbce10f4a625e235b10662cf
guo-ran <360112263@qq.com>
2020-03-31 15:53:45 +0800

    fix

--- yolov3/__init__.py
+++ yolov3/__init__.py
@@ -5,6 +5,6 @@ import glob
 def lib_path():
     dir_path = os.path.dirname(os.path.abspath(__file__))
     libs = glob.glob(os.path.join(dir_path, "liboneflow_yolov3.so"))
-    assert len(libs) > 0, f"no .so found in {dir_path}"
+    assert len(libs) > 0, "no .so found in {dir_path}"
     return libs[0]
 
