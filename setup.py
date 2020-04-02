215c80a12cdee5fd7f85595a6795b1ac087fce0c
guo-ran <360112263@qq.com>
2020-04-02 14:36:41 +0800

    setup darknet.so

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from setuptools import setup, find_packages
 
-package_data = {"oneflow_yolov3": ["liboneflow_yolov3.so"]}
+package_data = {"oneflow_yolov3": ["liboneflow_yolov3.so", "libdarknet.so"]}
 setup(
     name="oneflow_yolov3", packages=find_packages(), package_data=package_data,
 )
