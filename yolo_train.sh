47aa27fb7223024ac9922b72455a99145992836e
guo-ran <360112263@qq.com>
2020-07-06 01:38:05 +0800

    fix train.sh

--- yolo_train.sh
+++ yolo_train.sh
@@ -1,6 +1,5 @@
 model_dir=of_model/yolov3_model_python/
-image_path_file=data/with_dir_data_names
 export ONEFLOW_DEBUG_MODE=""
 
-python3 oneflow_yolov3/model/yolo_train.py --gpu_num_per_node=1 --batch_size=32  --image_path_file="gr_trainvalno5k.txt" --model_load_dir=$model_dir 
+python3 oneflow_yolov3/model/yolo_train.py --gpu_num_per_node=4 --batch_size=16 --base_lr=0.001 --image_path_file="test_trainvalno5k.txt" --total_batch_num=100 --model_load_dir=$model_dir --classes=80 --num_boxes=90
 
