e4248033e406813c4ae15b00c4c54a21815e285f
Flowingsun007 <flowingsun007@163.com>
2020-07-08 23:52:06 +0800

    add param for train coco

--- yolo_train.sh
+++ yolo_train.sh
@@ -5,8 +5,9 @@ export ONEFLOW_DEBUG_MODE=""
 
 python3 oneflow_yolov3/model/yolo_train.py \
 --gpu_num_per_node=1 --batch_size=4 --base_lr=0.001 \
---image_path_file="data/COCO/trainvalno5k.txt" \
 --total_batch_num=100 --model_load_dir=$model_dir \
 --classes=80 --num_boxes=90 --num_of_batches_in_snapshot=100 \
---model_save_dir="save_model"
+--model_save_dir="save_model" \
+--image_path_file = "test_trainvalno5k.txt"
+# --image_path_file="data/COCO/trainvalno5k.txt"
 
